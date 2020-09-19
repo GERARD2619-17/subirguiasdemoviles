b1e2b108000675a01881aa74269ffe210b8828ae
Gerardo Zelaya <57957674+GERARD2619-17@users.noreply.github.com>
2020-09-19 01:57:49 -0600

    segundo commit de laboratorio35

--- app/src/main/java/com/example/guia3/Anombre.java
+++ app/src/main/java/com/example/guia3/Anombre.java
@@ -44,7 +44,6 @@ public class Anombre extends AppCompatActivity implements View.OnClickListener {
         String nombrelista = editname.getText().toString();
         listanombres.add(nombrelista);
     }
-//laboratorio
     void enviarData(){
         final ProgressBar progressBar = findViewById(R.id.progressBar);
         progressBar.setProgress(0);
